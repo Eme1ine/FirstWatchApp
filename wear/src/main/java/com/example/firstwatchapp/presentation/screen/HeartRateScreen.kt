ed39f27bd813c96aefd18b59f2fb27507e637844
Eme1ine <emaugez@gmail.com>
2025-07-20 21:48:34 +0200

    Display never to 0.0

--- wear/src/main/java/com/example/firstwatchapp/presentation/screen/HeartRateScreen.kt
+++ wear/src/main/java/com/example/firstwatchapp/presentation/screen/HeartRateScreen.kt
@@ -2,6 +2,7 @@ package com.example.firstwatchapp.presentation.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -21,9 +22,25 @@ fun HeartRateScreen(heartRate : Float) {
     )
     {
         TimeText()
-        Text(
-            text = "Fréquence cardiaque : $heartRate bpm",
-            style = MaterialTheme.typography.title3
-        )
+        if (heartRate != 0f){
+            Column(horizontalAlignment = Alignment.CenterHorizontally)
+            {
+                Text(
+                    text = "Fréquence cardiaque :",
+                    style = MaterialTheme.typography.title3
+                )
+                Text(
+                    text = "$heartRate bpm",
+                    style = MaterialTheme.typography.title1
+                )
+            }
+        }
+        else
+        {
+            Text(
+                text = "Acquiring heart rate ...",
+                style = MaterialTheme.typography.title3
+            )
+        }
     }
 }
